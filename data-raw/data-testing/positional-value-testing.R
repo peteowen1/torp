71c959baa10f5e31d84e8f965126cc07d1fa92e2
peteowen1 <pete.owen1@hotmail.co.uk>
2023-09-07 11:06:39 +1000

    stat-models

--- data-raw/data-testing/positional-value-testing.R
+++ data-raw/data-testing/positional-value-testing.R
@@ -1,27 +1,264 @@
 ##### BASIC TESTING
+##### add to match pred data frame
+###
+plyr_mdl_df <-
+  pred_df %>%
+  left_join(
+    results %>%
+      dplyr::select(
+        match.matchId,
+        homeTeamScore.matchScore.totalScore, homeTeamScore.matchScore.goals, homeTeamScore.matchScore.behinds,
+        awayTeamScore.matchScore.totalScore, awayTeamScore.matchScore.goals, awayTeamScore.matchScore.behinds,
+        match.utcStartTime
+      ),
+    by = c("provider_id" = "match.matchId")
+  ) %>%
+  dplyr::mutate(
+    home_shots = homeTeamScore.matchScore.goals + homeTeamScore.matchScore.behinds,
+    away_shots = awayTeamScore.matchScore.goals + awayTeamScore.matchScore.behinds,
+    score_diff = ifelse(home_away == "home",
+                        homeTeamScore.matchScore.totalScore - awayTeamScore.matchScore.totalScore,
+                        awayTeamScore.matchScore.totalScore - homeTeamScore.matchScore.totalScore
+    ),
+    shot_diff = ifelse(home_away == "home",
+                       home_shots - away_shots,
+                       away_shots - home_shots
+    ),
+    team_shots = ifelse(home_away == "home",
+                        home_shots,
+                        away_shots
+    ),
+    shot_conv = ifelse(home_away == "home",
+                       homeTeamScore.matchScore.goals / home_shots,
+                       awayTeamScore.matchScore.goals / away_shots
+    ),
+    win = ifelse(score_diff > 0, 1, ifelse(score_diff == 0, 0.5, 0)),
+    ####
+    team_type_fac = as.factor(home_away),
+    total_score = homeTeamScore.matchScore.totalScore + awayTeamScore.matchScore.totalScore,
+    total_shots = home_shots + away_shots,
+    team_name.x = as.factor(team_name),
+    team_name.y = as.factor(opp_name),
+    weightz = exp(as.numeric(-(Sys.Date() - as.Date(match.utcStartTime))) / decay),
+    weightz = weightz / mean(weightz, na.rm = T),
+    row_id = paste0(player_id,substr(provider_id,5,8),round),
+    adj_score_diff = score_diff / 18
+  ) %>%
+  left_join(
+    torp_df_total, by = c('row_id'='row_id')
+    )
 
-afl_torp_diff_mdl <- mgcv::bam(score_diff ~
-                                  team_type_fac
-                               + I(torp_diff*1.1)
-                               + CB.x
-                               + BP.x
-                               + HBF.x
-                               + W.x
-                               + MIDS.x
-                               + RK.x
-                               + HFF.x
-                               + FP.x
-                               + CF.x
-                               + int.x
-                         #+ def.y + mid.y + fwd.y #+ int.y
-                         ,
-                     data = team_mdl_df %>% dplyr::filter(),
-                     family = "gaussian")
+##########################################
+plyr_torp_diff_mdl <- mgcv::bam(
+  score_diff ~
+    team_type_fac
+  + I(torp)
+  + I((pred_goals))#, bs = "ts")
+   + I((pred_behinds))#, bs = "ts")
+   + I((pred_kicks))#, bs = "ts")
+   + I((pred_handballs))#, bs = "ts") # +
+  # #+ s((pred_disposals), bs = "ts") # +
+  + I((pred_marks))#, bs = "ts")
+  + I((pred_bounces))#, bs = "ts") # * -
+  + I((pred_tackles))#, bs = "ts") # +
+  + I((pred_contested_possessions))#, bs = "ts") # ** +
+  + I((pred_uncontested_possessions))#, bs = "ts") # ** +
+  #+Is((pred_total_possessions), bs = "ts")
+  + I((pred_inside50s))#, bs = "ts") # *** +
+  + I((pred_marks_inside50))#, bs = "ts")
+  + I((pred_contested_marks))#, bs = "ts")
+  + I((pred_hitouts))#, bs = "ts")
+  # ### + s(scale(pred_one_percenters), bs='ts') # **
+   + I((pred_clangers))#, bs = "ts")
+   + I((pred_frees_for))#, bs = "ts") # ** -
+   + I((pred_frees_against))#, bs = "ts") # * +
+   + I((pred_rebound50s))#, bs = "ts")
+  # #+ I((pred_goal_assists))#, bs = "ts") # * +
+  # + I((pred_turnovers))#, bs = "ts") # * -
+  # + I((pred_intercepts))#, bs = "ts") # +
+   + I((pred_tackles_inside50))#, bs = "ts")
+  ###+ s((pred_shots_at_goal), bs = "ts")
+  # ### + s(scale(pred_score_involvements), bs='ts') # **
+  # + s((pred_clearances_centre_clearances), bs = "ts") # * -
+  # + s((pred_clearances_stoppage_clearances), bs = "ts")
+  # + s((pred_clearances_total_clearances), bs = "ts")
+  # + s((pred_extended_stats_effective_kicks), bs = "ts")
+  # #+ s(scale(pred_extended_stats_kick_to_handball_ratio), bs='ts')
+  # + s((pred_extended_stats_effective_disposals), bs = "ts")
+  # + s((pred_extended_stats_marks_on_lead), bs = "ts")
+  # + s((pred_extended_stats_intercept_marks), bs = "ts")
+  # + s((pred_extended_stats_hitouts_to_advantage), bs = "ts") # +
+  # + s((pred_extended_stats_ground_ball_gets), bs = "ts")
+  # + s((pred_extended_stats_f50ground_ball_gets), bs = "ts")
+  # ### + s(scale(pred_extended_stats_score_launches), bs='ts') # .
+  # + s((pred_extended_stats_pressure_acts), bs = "ts")
+  # + s((pred_extended_stats_def_half_pressure_acts), bs = "ts") # *** -
+  # + s((pred_extended_stats_spoils), bs = "ts") # * +
+  # + s((pred_extended_stats_ruck_contests), bs = "ts")
+  # + s((pred_extended_stats_contest_def_one_on_ones), bs = "ts")
+  # + s((pred_extended_stats_contest_def_losses), bs = "ts")
+  # + s((pred_extended_stats_contest_off_one_on_ones), bs = "ts")
+  # + s((pred_extended_stats_contest_off_wins), bs = "ts")
+  # #### + s(scale(pred_extended_stats_centre_bounce_attendances), bs='ts')
+  # #### + s(scale(pred_extended_stats_kickins), bs='ts')
+  # #### + s(scale(pred_extended_stats_kickins_playon), bs='ts')
+  # ############# BINOM
+  # + s((pred_time_on_ground_percentage), bs = "ts")
+  # + s((pred_disposal_efficiency), bs = "ts")
+  # + s((pred_goal_accuracy), bs = "ts")
+  # + s((pred_extended_stats_kick_efficiency), bs = "ts")
+  # + s((pred_extended_stats_contested_possession_rate), bs = "ts")
+  # + s((pred_extended_stats_hitout_win_percentage), bs = "ts")
+  # + s((pred_extended_stats_hitout_to_advantage_rate), bs = "ts")
+  # + s((pred_extended_stats_contest_def_loss_percentage), bs = "ts")
+  # + s((pred_extended_stats_contest_off_wins_percentage), bs = "ts")
+  ,
+  data = plyr_mdl_df %>% dplyr::filter(time_on_ground_percentage > 0.5),
+  select = T, discrete = T, nthreads = 4,
+  weights = pred_time_on_ground_percentage,
+  family = "gaussian"
+)
+
+
+summary(plyr_torp_diff_mdl)
+
+###
+plot(mgcViz::getViz(plyr_torp_diff_mdl))
+####
+
+####################################################
+###########################
+afl_torp_diff_mdl <- mgcv::bam(
+  score_diff ~
+    team_type_fac
+    + offset(torp_diff)
+    + s(scale(pred_goals), bs = "ts")
+    + s(scale(pred_behinds), bs = "ts")
+    + s(scale(pred_kicks), bs = "ts")
+    + s(scale(pred_handballs), bs = "ts") # +
+    + s(scale(pred_disposals), bs = "ts") # +
+    + s(scale(pred_marks), bs = "ts")
+    + s(scale(pred_bounces), bs = "ts") # * -
+    + s(scale(pred_tackles), bs = "ts") # +
+    + s(scale(pred_contested_possessions), bs = "ts") # ** +
+    + s(scale(pred_uncontested_possessions), bs = "ts") # ** +
+    + s(scale(pred_total_possessions), bs = "ts")
+    + s(scale(pred_inside50s), bs = "ts") # *** +
+    + s(scale(pred_marks_inside50), bs = "ts")
+    + s(scale(pred_contested_marks), bs = "ts")
+    + s(scale(pred_hitouts), bs = "ts")
+    ### + s(scale(pred_one_percenters), bs='ts') # **
+    + s(scale(pred_clangers), bs = "ts")
+    + s(scale(pred_frees_for), bs = "ts") # ** -
+    + s(scale(pred_frees_against), bs = "ts") # * +
+    + s(scale(pred_rebound50s), bs = "ts")
+    + s(scale(pred_goal_assists), bs = "ts") # * +
+    + s(scale(pred_turnovers), bs = "ts") # * -
+    + s(scale(pred_intercepts), bs = "ts") # +
+    + s(scale(pred_tackles_inside50), bs = "ts")
+    + s(scale(pred_shots_at_goal), bs = "ts")
+    ### + s(scale(pred_score_involvements), bs='ts') # **
+    + s(scale(pred_clearances_centre_clearances), bs = "ts") # * -
+    + s(scale(pred_clearances_stoppage_clearances), bs = "ts")
+    + s(scale(pred_clearances_total_clearances), bs = "ts")
+    + s(scale(pred_extended_stats_effective_kicks), bs = "ts")
+    #+ s(scale(pred_extended_stats_kick_to_handball_ratio), bs='ts')
+    + s(scale(pred_extended_stats_effective_disposals), bs = "ts")
+    + s(scale(pred_extended_stats_marks_on_lead), bs = "ts")
+    + s(scale(pred_extended_stats_intercept_marks), bs = "ts")
+    + s(scale(pred_extended_stats_hitouts_to_advantage), bs = "ts") # +
+    + s(scale(pred_extended_stats_ground_ball_gets), bs = "ts")
+    + s(scale(pred_extended_stats_f50ground_ball_gets), bs = "ts")
+    ### + s(scale(pred_extended_stats_score_launches), bs='ts') # .
+    + s(scale(pred_extended_stats_pressure_acts), bs = "ts")
+    + s(scale(pred_extended_stats_def_half_pressure_acts), bs = "ts") # *** -
+    + s(scale(pred_extended_stats_spoils), bs = "ts") # * +
+    + s(scale(pred_extended_stats_ruck_contests), bs = "ts")
+    + s(scale(pred_extended_stats_contest_def_one_on_ones), bs = "ts")
+    + s(scale(pred_extended_stats_contest_def_losses), bs = "ts")
+    + s(scale(pred_extended_stats_contest_off_one_on_ones), bs = "ts")
+    + s(scale(pred_extended_stats_contest_off_wins), bs = "ts")
+    #### + s(scale(pred_extended_stats_centre_bounce_attendances), bs='ts')
+    #### + s(scale(pred_extended_stats_kickins), bs='ts')
+    #### + s(scale(pred_extended_stats_kickins_playon), bs='ts')
+    ############# BINOM
+    + s(scale(pred_time_on_ground_percentage), bs = "ts")
+    + s(scale(pred_disposal_efficiency), bs = "ts")
+    + s(scale(pred_goal_accuracy), bs = "ts")
+    + s(scale(pred_extended_stats_kick_efficiency), bs = "ts")
+    + s(scale(pred_extended_stats_contested_possession_rate), bs = "ts")
+    + s(scale(pred_extended_stats_hitout_win_percentage), bs = "ts")
+    + s(scale(pred_extended_stats_hitout_to_advantage_rate), bs = "ts")
+    + s(scale(pred_extended_stats_contest_def_loss_percentage), bs = "ts")
+    + s(scale(pred_extended_stats_contest_off_wins_percentage), bs = "ts")
+    ### positions
+    + s(CB.x, bs = "ts")
+    + s(BP.x, bs = "ts")
+    + s(HBF.x, bs = "ts")
+    + s(W.x, bs = "ts")
+    + s(MIDS.x, bs = "ts")
+    + s(RK.x, bs = "ts")
+    + s(HFF.x, bs = "ts")
+    + s(FP.x, bs = "ts")
+    + s(CF.x, bs = "ts")
+    + s(int.x, bs = "ts")
+  #+ def.y + mid.y + fwd.y #+ int.y
+  ,
+  data = team_mdl_df %>% dplyr::filter(),
+  select = T, discrete = T, nthreads = 4,
+  family = "gaussian"
+)
 
 
 summary(afl_torp_diff_mdl)
 
+####
+plot(mgcViz::getViz(afl_torp_diff_mdl))
+
 team_mdl_df$pred_score_difference <- predict(afl_torp_diff_mdl, team_mdl_df, type = "response")
+
+#############
+pred_df$team_type_fac <- as.factor("home")
+pred_df$torp_diff <- 0
+
+pred_df$pred_val <- mgcv::predict.bam(afl_torp_diff_mdl, newdata = pred_df, type = "response")
+
+
+###################################
+##### BASIC TESTING
+
+afl_torp_diff_mdl <- mgcv::bam(
+  score_diff ~
+    team_type_fac
+    + I(torp_diff)
+    + CB.x
+    + BP.x
+    + HBF.x
+    + W.x
+    + MIDS.x
+    + RK.x
+    + HFF.x
+    + FP.x
+    + CF.x
+    + int.x
+  # + key_def.x
+  # + med_def.x
+  # + midfield.x
+  # + mid_fwd.x
+  # + med_fwd.x
+  # + key_fwd.x
+  # + rucks.x
+  #+ def.y + mid.y + fwd.y #+ int.y
+  ,
+  data = team_mdl_df %>% dplyr::filter(),
+  select = T, # discrete = T,
+  nthreads = 4,
+  family = "gaussian"
+)
+
+
+summary(afl_torp_diff_mdl)
+
 
 ##### BASIC TESTING
 afl_torp_diff_win_mdl <- mgcv::bam(
--- data-raw/wt_av_modelling.R
+++ data-raw/wt_av_modelling.R
@@ -1,103 +1,299 @@
 library(furrr)
 library(tidyverse)
+library(fitzRoy)
+devtools::load_all()
+
+minmax <- function(x, na.rm = TRUE) {
+  return((x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE)))
+}
 
 # fil_val <- 24
 decay <- 365
 
-ps23 <- fetch_player_stats_afl(2023) %>% janitor::remove_constant() %>% janitor::clean_names()
-ps22 <- fetch_player_stats_afl(2022) %>% janitor::remove_constant() %>% janitor::clean_names()
-ps21 <- fetch_player_stats_afl(2021) %>% janitor::remove_constant() %>% janitor::clean_names()
-ps20 <- fetch_player_stats_afl(2020) %>% janitor::remove_constant() %>% janitor::clean_names()
-pstot <- bind_rows(ps20,ps21,ps22,ps23) ### columns 22 to 88
+tictoc::tic()
+
+pl_details <- fetch_player_details_afl(2023)
+
+ps23 <- fetch_player_stats_afl(2023) %>%
+  janitor::remove_constant() %>%
+  janitor::clean_names()
+ps22 <- fetch_player_stats_afl(2022) %>%
+  janitor::remove_constant() %>%
+  janitor::clean_names()
+ps21 <- fetch_player_stats_afl(2021) %>%
+  janitor::remove_constant() %>%
+  janitor::clean_names()
+ps20 <- fetch_player_stats_afl(2020) %>%
+  janitor::remove_constant() %>%
+  janitor::clean_names()
+pstot <- bind_rows(ps20, ps21, ps22, ps23)
+
+### columns 22 to 88
 
 ##############################
 cols <- colnames(pstot)[18:79]
-cols <- cols[!cols %in% c('dream_team_points','rating_points','metres_gained','last_updated')]
+cols <- cols[!cols %in% c(
+  "dream_team_points", "rating_points", "metres_gained", "last_updated",
+  "extended_stats_centre_bounce_attendances", "extended_stats_kickins", "extended_stats_kickins_playon"
+)]
 
-cols_binom <- c('time_on_ground_percentage','disposal_efficiency','goal_accuracy',
-                'extended_stats_kick_efficiency','extended_stats_contested_possession_rate','extended_stats_hitout_win_percentage',
-                'extended_stats_hitout_to_advantage_rate','extended_stats_contest_def_loss_percentage','extended_stats_contest_off_wins_percentage')
-cols_pois <- setdiff(cols,cols_binom)
+cols_binom <- c(
+  "time_on_ground_percentage", "disposal_efficiency", "goal_accuracy",
+  "extended_stats_kick_efficiency", "extended_stats_contested_possession_rate", "extended_stats_hitout_win_percentage",
+  "extended_stats_hitout_to_advantage_rate", "extended_stats_contest_def_loss_percentage", "extended_stats_contest_off_wins_percentage"
+)
+cols_pois <- setdiff(cols, cols_binom)
 
+################
+wav_data <- function(df, fil_val, model_col, decay = 365) {
+  df$season_round <- paste0(substr(df$provider_id, 5, 8), substr(df$provider_id, 12, 13))
 
-wav_data <- function(df,fil_val,model_col,decay = 150){
-
-  df$season_round <- paste0(substr(df$provider_id,5,8),substr(df$provider_id,12,13))
+  df$opponent_name <- ifelse(df$team_status == "home", df$away_team_name, df$home_team_name)
 
   df_old <- df %>%
     filter(season_round < fil_val) %>%
-    mutate(weight_gm = exp(as.numeric(-(max(as.Date(.data$utc_start_time)) - as.Date(.data$utc_start_time)))/ decay)
-    )
+    mutate(weight_gm = exp(as.numeric(-(max(as.Date(.data$utc_start_time)) - as.Date(.data$utc_start_time))) / decay))
 
   df_cur <- df %>% filter(season_round == fil_val)
 
+  ###
   df_player <-
     df_old %>%
-    group_by(player_id = player_player_player_player_id,
-             #player_name = paste(player_player_player_given_name,player_player_player_surname)
-             ) %>%
-    summarise(wt_avg = round(sum(.data[[model_col]]*.data$weight_gm)/sum(.data$weight_gm),3),
-              log_wt_avg = log(wt_avg+1),
-              wt_gms = sum(.data$weight_gm)
+    group_by(
+      player_id = player_player_player_player_id,
+      # player_name = paste(player_player_player_given_name,player_player_player_surname)
+    ) %>%
+    summarise(
+      wt_avg = round(sum(.data[[model_col]] * .data$weight_gm, na.rm = T) / sum(.data$weight_gm, na.rm = T), 3),
+      wt_avg = replace_na(wt_avg, 0),
+      log_wt_avg = log(wt_avg + 1),
+      wt_gms = sum(.data$weight_gm, na.rm = T)
     ) %>%
     ungroup()
 
+  ###
+  df_team <-
+    df_old %>%
+    group_by(team_name) %>%
+    summarise(
+      wt_avg_team = round(sum(.data[[model_col]] * .data$weight_gm, na.rm = T) / sum(.data$weight_gm, na.rm = T), 3),
+      wt_avg_team = replace_na(wt_avg_team, 0),
+      log_wt_avg_team = log(wt_avg_team + 1),
+      wt_gms_team = sum(.data$weight_gm, na.rm = T)
+    ) %>%
+    ungroup()
+
+  ###
+  df_opp <-
+    df_old %>%
+    group_by(opponent_name) %>%
+    summarise(
+      wt_avg_opp = round(sum(.data[[model_col]] * .data$weight_gm, na.rm = T) / sum(.data$weight_gm, na.rm = T), 3),
+      wt_avg_opp = replace_na(wt_avg_opp, 0),
+      log_wt_avg_opp = log(wt_avg_opp + 1),
+      wt_gms_opp = sum(.data$weight_gm, na.rm = T)
+    ) %>%
+    ungroup()
+
+  ###########
   df_tot <-
     df_player %>%
-    right_join(df_cur, by = c('player_id'='player_player_player_player_id')) %>%
-    mutate(position = as.factor(substr(player_player_position,1,2))) %>%
-    relocate(any_of(model_col),position)
+    right_join(df_cur, by = c("player_id" = "player_player_player_player_id")) %>%
+    left_join(df_team, by = c("team_name" = "team_name")) %>%
+    left_join(df_opp, by = c("opponent_name" = "opponent_name")) %>%
+    mutate(
+      round = round_round_number,
+      season = as.factor(substr(provider_id,5,8)),
+      venue = as.factor(venue_name),
+      aest_start = with_tz(as_datetime(utc_start_time),"Australia/Brisbane"),
+      date_numeric = as.numeric(aest_start),
+      aest_hour = (hour(aest_start)*60+minute(aest_start))/60,
+      aest_day = wday(aest_start,label = TRUE),
+      position = as.factor(substr(player_player_position, 1, 2)),
+      home_away = as.factor(team_status),
+      player_name = paste(player_player_player_given_name, player_player_player_surname)
+    ) %>%
+    relocate(any_of(model_col), position)
 
   return(df_tot)
 }
 
-############
+############ POIS
 stat_list <- list()
 
 tictoc::tic()
 for (i in cols_pois) {
+  df_mdl <- furrr::future_map(
+    paste0(rep(2021:2023, each = 27), rep(sprintf("%02d", 1:27), times = 3)),
+    ~ wav_data(pstot,
+      fil_val = .,
+      model_col = i
+    )
+  ) %>%
+    purrr::list_rbind()
 
-df_mdl <- furrr::future_map(paste0(2023,sprintf('%02d',1:24)),
-               ~wav_data(pstot,
-                         fil_val = .,
-                         model_col = i )) %>%
-                 purrr::list_rbind()
 
+  mdl <- mgcv::bam(
+    as.formula(paste0(
+      i,
+      " ~ ti(log_wt_avg,wt_gms, bs = 'ts') + s(log_wt_avg, bs='ts') + s(wt_gms, bs='ts')",
+      "+ s(position,bs='re') + s(log_wt_avg_team, bs='ts') + s(log_wt_avg_opp, bs='ts') + s(home_away, bs='re')",
+      "+ s(venue, bs='re') + s(round, bs='ts') + s(date_numeric, bs='ts', m=1) + s(aest_day, bs='re') + s(aest_hour, bs='ts')"
+    )),
+    data = df_mdl, family = poisson(),
+    select = T, discrete = T, nthreads = 4,
+  )
 
-mdl <- mgcv::bam(as.formula(paste0(i,
-                                   " ~ ti(log_wt_avg,wt_gms, bs = 'ts') + s(log_wt_avg, bs='ts') + s(wt_gms, bs='ts') + s(position,bs='re')"))
-                 , data = df_mdl, family = poisson()
-                 , select = T, discrete = T , nthreads = 4,
-)
+  model_preds <- tibble(
+    player_id = df_mdl$player_id,
+    player_name = df_mdl$player_name,
+    player_position = df_mdl$player_player_position,
+    provider_id = df_mdl$provider_id,
+    round = df_mdl$round_round_number,
+    home_away = df_mdl$home_away,
+    team_name = df_mdl$team_name,
+    opp_name = df_mdl$opponent_name,
+    "{i}" := df_mdl[[i]],
+    "pred_{i}" := mgcv::predict.bam(mdl, newdata = df_mdl, type = "response"),
+    "wt_avg_{i}" := df_mdl$wt_avg,
+    "wt_gms_{i}" := df_mdl$wt_gms,
+    "wt_avg_team_{i}" := df_mdl$wt_avg_team,
+    "wt_avg_opp_{i}" := df_mdl$wt_avg_opp
+  )
 
-model_preds <- tibble(
-                      player_id = df_mdl$player_id,
-                      provider_id = df_mdl$provider_id,
-                      team_id = df_mdl$team_id,
-                      "{i}" := df_mdl[[i]],
-                      "pred_{i}" := mgcv::predict.bam(mdl,newdata = df_mdl, type='response'),
-                      "wt_avg_{i}" := df_mdl$wt_avg,
-                      "wt_gms_{i}" := df_mdl$wt_gms,
-                      )
+  stat_list[[i]] <- model_preds
 
-stat_list[[i]] <- model_preds
-
-saveRDS(mdl, glue::glue('./data-raw/stat-models/{i}.rds'))
-print(i)
+  saveRDS(mdl, glue::glue("./data-raw/stat-models/{i}.rds"))
+  print(i)
 }
+
 tictoc::toc()
 
-pred_df <- stat_list %>% reduce(left_join, by= c('player_id','provider_id','team_id'))
+############ BINOM
+# stat_list_binom <- list()
+
+tictoc::tic()
+for (i in cols_binom) {
+  df_mdl <- furrr::future_map(
+    paste0(rep(2021:2023, each = 27), rep(sprintf("%02d", 1:27), times = 3)),
+    ~ wav_data(pstot %>%
+                 mutate("{i}" := .data[[i]] / 100),
+      fil_val = .,
+      model_col = i
+    )
+  ) %>%
+    purrr::list_rbind()
+
+
+  mdl <- mgcv::bam(
+    as.formula(paste0(
+      i,
+      " ~ ti(log_wt_avg,wt_gms, bs = 'ts') + s(log_wt_avg, bs='ts') + s(wt_gms, bs='ts')",
+      "+ s(position,bs='re') + s(log_wt_avg_team, bs='ts') + s(log_wt_avg_opp, bs='ts') + s(home_away, bs='re')",
+      "+ s(venue, bs='re') + s(round, bs='ts') + s(date_numeric, bs='ts', m=1) + s(aest_day, bs='re') + s(aest_hour, bs='ts')"
+    )),
+    data = df_mdl, family = binomial(),
+    select = T, discrete = T, nthreads = 4,
+  )
+
+  model_preds <- tibble(
+    player_id = df_mdl$player_id,
+    player_name = df_mdl$player_name,
+    player_position = df_mdl$player_player_position,
+    provider_id = df_mdl$provider_id,
+    round = df_mdl$round_round_number,
+    home_away = df_mdl$home_away,
+    team_name = df_mdl$team_name,
+    opp_name = df_mdl$opponent_name,
+    "{i}" := df_mdl[[i]],
+    "pred_{i}" := mgcv::predict.bam(mdl, newdata = df_mdl, type = "response"),
+    "wt_avg_{i}" := df_mdl$wt_avg,
+    "wt_gms_{i}" := df_mdl$wt_gms,
+    "wt_avg_team_{i}" := df_mdl$wt_avg_team,
+    "wt_avg_opp_{i}" := df_mdl$wt_avg_opp
+  )
+
+  stat_list[[i]] <- model_preds
+
+  saveRDS(mdl, glue::glue("./data-raw/stat-models/{i}.rds"))
+  print(i)
+}
+
+tictoc::toc()
+
+### combine
+pred_df <- stat_list %>% reduce(left_join, by = c("player_id", "player_name","player_position",
+                                                  "provider_id", "round", "home_away" ,"team_name", "opp_name"))
 pred_df
 
-###############################
-# plot(mgcViz::getViz(mdl))
-# mixedup::extract_random_effects(mdl) %>% View()
-model_val <- 'extended_stats_ruck_contests'
-mdl <- readRDS(paste0('./data-raw/stat-models/',model_val,'.rds'))
+saveRDS(pred_df, "./data-raw/stat_pred_df.rds")
+tictoc::toc()
+
+#################################################################
+
+pred_df <- readRDS("./data-raw/stat_pred_df.rds")
+
+##############################
+model_val <- "disposals"
+pred_model_val <- paste0("pred_", model_val)
+wt_avg_model_val <- paste0("wt_avg_", model_val)
+
+mdl <- readRDS(paste0("./data-raw/stat-models/", model_val, ".rds"))
 summary(mdl)
-mixedup::extract_random_effects(mdl) %>% arrange(-value)
-plot(mgcViz::getViz(mdl))
+mixedup::extract_random_effects(mdl) %>% arrange(-value) #%>% View()
+# plot(mgcViz::getViz(mdl))
 
 
+############
+stat_perf <- function(var) {
+  model_val <- var
+  pred_model_val <- paste0("pred_", model_val)
+  wt_avg_model_val <- paste0("wt_avg_", model_val)
+  sim_model_val <- paste0("sim_", model_val)
 
+  test_df <- pred_df %>% filter(!is.na(.data[[pred_model_val]]))
+  test_df <- test_df %>%
+    mutate("sim_{model_val}" := rpois(nrow(test_df), .data[[pred_model_val]]))
+
+  df <- tibble(
+    var = paste(var),
+    sim_rmse = ModelMetrics::rmse(test_df %>% pull(sim_model_val), test_df %>% pull(pred_model_val)),
+    model_rmse = ModelMetrics::rmse(test_df %>% pull(model_val), test_df %>% pull(pred_model_val)),
+    wt_avg_rmse = ModelMetrics::rmse(test_df %>% pull(model_val), test_df %>% pull(wt_avg_model_val)),
+    naive_rmse = ModelMetrics::rmse(test_df %>% pull(model_val), rep(mean(test_df %>% pull(model_val)), nrow(test_df)))
+  )
+
+  return(df)
+}
+
+all_perf <- map(c(cols_pois,cols_binom), ~ stat_perf(.)) %>% list_rbind()
+
+##### add to match pred data frame
+team_preds <- pred_df %>%
+  group_by(provider_id, team_name, opp_name) %>%
+  summarise_if(is.numeric, sum, na.rm = TRUE) # %>% View()
+
+team_mdl_df <- team_mdl_df %>%
+  left_join(team_preds, by = c("providerId" = "provider_id", "teamName.x" = "team_name")) # %>% View()
+
+###
+colnames(team_mdl_df)[str_detect(colnames(team_mdl_df), "pred")]
+
+###
+pl_df_final <-
+  pl_details %>%
+  select(providerId,position) %>%
+  left_join(pred_df %>% filter(substr(provider_id,1,13)=='CD_M202301423'),
+                               by = c("providerId" = "player_id")) #%>%
+  #filter(position == "RUCK") %>%
+  # arrange(-pred_val) %>%
+  # relocate(pred_val) #%>%
+  #select(1:10) %>%
+  # group_by(position) %>%
+  # summarise(sqrt(var(pred_val, na.rm=T))) %>%
+  #View()
+
+########
+pl_df_final %>%
+  group_by(position) %>%
+  summarise(across(starts_with("pred"),~mean(.x, na.rm=T))) %>% view()
