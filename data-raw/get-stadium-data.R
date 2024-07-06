865d09ec6af536816216e9cd9ef916d6e07d782f
Peter Owen <pete.owen1@hotmail.co.uk>
2024-07-06 19:09:18 +1000

    stadium data

---
+++ data-raw/get-stadium-data.R
@@ -0,0 +1,99 @@
+library(rvest)
+library(dplyr)
+library(tidyr)
+library(osmdata)
+library(stringr)
+
+# URL of the Wikipedia page
+url <- "https://en.wikipedia.org/wiki/List_of_Australian_Football_League_grounds"
+
+# Read the HTML content of the page
+page <- read_html(url)
+
+# Extract the tables
+tables <- page %>% html_nodes("table.wikitable")
+
+# Function to clean and process each table
+process_current_table <- function(table) {
+  df <- table %>% html_table(fill = TRUE) %>% .[[1]]
+  df %>%
+    select(Ground, City, `State/territory`, Capacity, `First used`, Games, `Current tenant(s)`) %>%
+    rename(
+      State_Territory = `State/territory`,
+      First_Used = `First used`,
+      Current_Tenants = `Current tenant(s)`
+    ) %>%
+    mutate(across(everything(), ~str_replace_all(., "\\[.*?\\]", ""))) %>%
+    mutate(Latitude = NA, Longitude = NA)
+}
+
+process_former_table <- function(table) {
+  df <- table %>% html_table(fill = TRUE) %>% .[[1]]
+  df %>%
+    select(Ground, City, State, Capacity, `First used`, Games, `Current tenant(s)` = `Tenant(s)`) %>%
+    rename(
+      State_Territory = State,
+      First_Used = `First used`,
+      Current_Tenants = `Current tenant(s)`
+    ) %>%
+    mutate(across(everything(), ~str_replace_all(., "\\[.*?\\]", ""))) %>%
+    mutate(Latitude = NA, Longitude = NA)
+}
+
+process_other_table <- function(table) {
+  df <- table %>% html_table(fill = TRUE) %>% .[[1]]
+  df %>%
+    select(Ground, City, State = `State/Country`, Capacity, Games, `First used` = `Last used`, `Current tenant(s)` = Uses) %>%
+    rename(
+      State_Territory = State,
+      First_Used = `First used`,
+      Current_Tenants = `Current tenant(s)`
+    ) %>%
+    mutate(across(everything(), ~str_replace_all(., "\\[.*?\\]", ""))) %>%
+    mutate(Latitude = NA, Longitude = NA)
+}
+
+# Process each table
+current_grounds <- process_current_table(tables[1])
+former_major_grounds <- process_former_table(tables[2])
+other_minor_grounds <- process_other_table(tables[3])
+
+# Combine all tables into one
+all_grounds <- bind_rows(current_grounds, former_major_grounds, other_minor_grounds)
+
+# Function to get latitude and longitude using OSM
+get_lat_lon <- function(place) {
+  res <- tryCatch(
+    {
+      opq(place) %>%
+        add_osm_feature(key = 'place', value = 'square') %>%
+        osmdata_sf()
+    },
+    error = function(e) {
+      return(NULL)
+    }
+  )
+
+  if (!is.null(res) && !is.null(res$bb)) {
+    bb <- strsplit(res$bb, ",")[[1]]
+    lat <- (as.numeric(bb[1]) + as.numeric(bb[3])) / 2
+    lon <- (as.numeric(bb[2]) + as.numeric(bb[4])) / 2
+    return(c(lat, lon))
+  } else {
+    return(c(NA, NA))
+  }
+}
+
+# Loop through each ground to get the coordinates
+for (i in 1:nrow(all_grounds)) {
+  place <- paste(all_grounds$Ground[i], all_grounds$City[i], all_grounds$State_Territory[i], "Australia", sep=", ")
+  coords <- get_lat_lon(place)
+  all_grounds$Latitude[i] <- coords[1]
+  all_grounds$Longitude[i] <- coords[2]
+
+  # Print progress
+  print(paste("Processed:", all_grounds$Ground[i], "- Row", i, "of", nrow(all_grounds)))
+}
+
+# Display the cleaned data
+print(all_grounds)
